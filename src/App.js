5004a76f4e40c5f0161c36a630777ca6b9859657
Hadrian Hughes <h.hughes@amaze.com>
2020-01-20 19:53:03 +0000

    Create Homepage component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import Page from './components/Page';
-import Heading from './components/Heading';
-import NavLinks from './components/NavLinks';
 import GlobalStyles from './styles/global';
+import Home from './pages/Home';
 
 const App = ({ context }) => (
   <Fragment>
     <GlobalStyles />
-    <Page>
-      <Heading lines={[context.headingLineOne, context.headingLineTwo]} />
-      <NavLinks links={context.navLinks} />
-    </Page>
+    <Home context={context.home} />
   </Fragment>
 );
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Page from '../../components/Page';
+import Heading from '../../components/Heading';
+import NavLinks from '../../components/NavLinks';
+
+const Home = ({ context }) => (
+  <Page>
+    <Heading lines={[context.headingLineOne, context.headingLineTwo]} />
+    <NavLinks links={context.navLinks} />
+  </Page>
+);
+
+Home.propTypes = {
+  context: PropTypes.object
+};
+
+Home.defaultProps = {
+  context: {}
+};
+
+export default Home;
