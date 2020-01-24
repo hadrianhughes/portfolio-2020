fa5f71be5bb5464583c8881e1a06fff800476385
Hadrian Hughes <hadrian.hughes@amazerealise.com>
2020-01-24 09:06:00 +0000

    Only use typewriter animation on homepage heading

--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -18,19 +18,22 @@ export const H1 = styled.h1`${HStyles}`;
 export const H2 = styled.h2`${HStyles}`;
 
 export const Span = styled.span`
-  animation: 1s typewriter${props => props.index} steps(${props => props.length});
-  animation-delay: ${props => props.index}s;
-  animation-fill-mode: forwards;
   display: block;
   margin: 10px auto;
   overflow: hidden;
   text-align: center;
   white-space: nowrap;
-  width: 0;
 
-  ${minWidth('medium')} {
-    animation-name: desktop_typewriter${props => props.index};
-  }
+  ${props => props.main ? `
+    animation: 1s typewriter${props.index} steps(${props.length});
+    animation-delay: ${props.index}s;
+    animation-fill-mode: forwards;
+    width: 0;
+
+    ${minWidth('medium')} {
+      animation-name: desktop_typewriter${props.index};
+    }
+  ` : ''}
 
   @keyframes typewriter${props => props.index} {
     0% { width: 0px; }
