87e48daa22325efdc3a464d402411642bab22804
Hadrian Hughes <h.hughes@amaze.com>
2020-02-15 16:24:15 +0000

    Add Safari polyfill for smooth scrolling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import smoothScroll from 'smoothscroll-polyfill';
 import App from './App';
+
+smoothScroll.polyfill();
 
 const el = document.getElementById('root');
 
