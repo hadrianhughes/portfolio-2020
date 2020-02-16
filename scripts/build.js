cbdce73ea6908938b6863b7f4556d76497249972
Hadrian Hughes <h.hughes@amaze.com>
2020-02-16 00:41:47 +0000

    Server side render styles to avoid FOUC

--- scripts/build.js
+++ scripts/build.js
@@ -4,13 +4,14 @@ const path = require('path');
 const fs = require('fs');
 const React = require('react');
 const { renderToString } = require('react-dom/server');
+const { ServerStyleSheet } = require('styled-components');
 const App = require('../src/App').default;
 const { oneLine } = require('common-tags');
 
 const BUILD_DIR = path.resolve(__dirname, '../build');
 const content = require('../content.json');
 
-const renderFullPage = (context, html) => oneLine`
+const renderFullPage = (context, rendered) => oneLine`
   <!DOCTYPE html>
   <html lang="en">
   <head>
@@ -20,19 +21,29 @@ const renderFullPage = (context, html) => oneLine`
     <title>${context.title}</title>
     <script>window.APP_CONTEXT = ${JSON.stringify(context)};</script>
     <link href="https://fonts.googleapis.com/css?family=Merriweather|Bitter&display=swap" rel="stylesheet" />
+    <style>${rendered.styleSheet}</style>
   </head>
   <body>
-    <div id="root">${html}</div>
+    <div id="root">${rendered.html}</div>
     <script src="/bundle.js"></script>
   </body>
   </html>
 `;
 
-const html = renderFullPage(content,
-  renderToString(
-    React.createElement(App, { context: content })
-  )
-);
+const html = renderFullPage(content, (() => {
+  const styleSheet = new ServerStyleSheet();
+
+  const markup = renderToString(
+    styleSheet.collectStyles(
+      React.createElement(App, { context: content })
+    )
+  );
+
+  const styleTags = styleSheet.getStyleTags();
+  styleSheet.seal();
+
+  return { html: markup, styleSheet: styleTags };
+})());
 
 fs.writeFile(`${BUILD_DIR}/index.html`, html, () => {
   console.info('Rendered index.html');
